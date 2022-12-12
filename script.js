7c06900995afc7e46ab9ae260bdb9d5458fcddad
KleinerKaiser <iljazaicevs@outlook.com>
2022-12-12 22:13:28 +0200

    client-write db added

--- script.js
+++ script.js
@@ -1,7 +1,59 @@
 // NOTE: RUN WITH HTTP://, NOT FILE://
-window.addEventListener("load", () => {
-    fetch("server.php", { method : "POST" })
-    .then(res => res.text()).then((txt) => {
-      document.getElementById("demo").innerHTML = txt;
-    });
+function FETCHDB() {
+  var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.open("POST", "server.php", true);
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState === 4 || this.status === 200) {
+      document.getElementById("demo").innerHTML = xmlhttp.responseText;
+    }
+  };
+  xmlhttp.send("dataSent="+"request_DB");
+}
+
+function WRITEDB(){
+  "use strict";   
+
+
+  let payload={
+    "org_name":org.value,
+    "event_name":evname.value,
+    "contacts":cont.value,
+    "start_date":start.value,
+    "end_date":end.value
+  };
+  let DT_regex=/(:)/g;
+  payload["start_date"]=payload["start_date"].replace(DT_regex, '@');
+  payload["end_date"]=payload["end_date"].replace(DT_regex, '@');
+  Object.keys(payload).forEach(key => {
+    if (payload[key]=="" || payload[key]==null)
+    {
+      payload[key]="NULL";
+    }
   });
+  let payloadString=JSON.stringify(payload);
+  console.log(payloadString);
+  //var regex=/([{-}",=])+()/g;
+
+  //payloadString = payloadString.replace(regex, " ");
+  //regex=/( : )/g;
+  //payloadString=payloadString.replace(regex, ':');
+
+  let regex=/([{-}"])/g
+  payloadString=payloadString.replace(regex, '');
+  payloadString=payloadString.replace(DT_regex, '=');
+  DT_regex=/(@)/g;
+  payloadString=payloadString.replace(DT_regex, ':');
+  console.log(payloadString);
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("POST", "server.php", true);
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.onreadystatechange = function() {
+      if (this.readyState === 4 || this.status === 200){ 
+          console.log(this.responseText); // echo from php
+      }       
+  };
+  xmlhttp.send("dataSent="+payloadString);
+  }
+
--- server.php
+++ server.php
@@ -2,27 +2,115 @@
 // (A) SETTINGS - CHANGE TO YOUR OWN !
 error_reporting(E_ALL & ~E_NOTICE);
 define("DB_HOST", "localhost");
-define("DB_NAME", "users");
+define("DB_NAME", "jauniesu_centrs");
 define("DB_CHARSET", "utf8");
 define("DB_USER", "root");
-define("DB_PASSWORD", "password");
+define("DB_PASSWORD", "arioliev3");
 
 // (B) CONNECT TO DATABASE
-try {
-  $pdo = new PDO(
-    "mysql:host=" . DB_HOST . ";charset=" . DB_CHARSET . ";dbname=" . DB_NAME, 
-    DB_USER, DB_PASSWORD
-  );
-} catch (Exception $ex) { exit($ex->getMessage()); }
 
-// (C) GET USERS
-$stmt = $pdo->prepare("SELECT * FROM `all_users`");
-$stmt->execute();
-$users = $stmt->fetchAll();
-foreach ($users as $u) {
-  printf("<div>[%u] %s</div>", $u['user_id'], $u['user_name']);
+$a=$_POST;
+
+function responseDBfetch()
+{
+
+  $pdo;
+  try {
+    $pdo = new PDO(
+      "mysql:host=" . DB_HOST . ";charset=" . DB_CHARSET . ";dbname=" . DB_NAME, 
+      DB_USER, DB_PASSWORD
+    );
+  } catch (Exception $ex) { exit($ex->getMessage()); }
+
+  $stmt = $pdo->prepare("SELECT * FROM `events`");
+  $stmt->execute();
+  $table = $stmt->fetchAll();
+  $retVal="";
+  foreach ($table as &$row)
+  {
+    foreach ($row as &$col)
+    {
+      $retVal = $retVal . ($col."\n");
+    }
+  }
+  return $retVal;
+};
+
+
+function writeToDB($data)
+{
+  $pdo;
+  try {
+    $pdo = new PDO(
+      "mysql:host=" . DB_HOST . ";charset=" . DB_CHARSET . ";dbname=" . DB_NAME, 
+      DB_USER, DB_PASSWORD
+    );
+  } catch (Exception $ex) { exit($ex->getMessage()); }
+
+  $org=$data["org_name"];
+  $evnt=$data["event_name"];
+  $cont=$data["contacts"];
+  $start=$data["start_date"];
+  $end=$data["end_date"];
+  echo($start);
+  //echo("insert into events(org_name, event_name, contacts, start_date, end_date)
+  //values({$org}, {$evnt}, {$cont}, {$start}, {$end});");
+  $stmt = $pdo->prepare("insert into events(org_name, event_name, contacts, start_date, end_date)
+  values({$org}, {$evnt}, {$cont}, {$start}, {$end});");
+  $stmt->execute();
+  $table = $stmt->fetchAll();
+  $retVal="";
+  foreach ($table as &$row)
+  {
+    foreach ($row as &$col)
+    {
+      $retVal = $retVal . ($col."\n");
+    }
+  }
+  echo($retVal);
 }
 
-// (D) CLOSE DATABASE CONNECTION
+if ($a["dataSent"]=="request_DB")
+{
+  print_r(responseDBfetch());
+}
+else
+{
+  $sendData=[
+    "org_name" => "",
+    "event_name" => "",
+    "contacts" => "",
+    "start_date" => "",
+    "end_date" => "",
+  ];
+
+  
+
+  $allData=explode(",", $a["dataSent"]);
+
+  for ($i=0; $i<count($allData); $i++)
+  {
+    echo($allData[$i]."\n");
+
+    $divided=explode('=', $allData[$i]);
+    if ($sendData[$divided[0]]!==NULL)
+    {
+      $sendData[$divided[0]]="'".$divided[1]."'";
+    }
+  }
+
+  writeToDB($sendData);   
+}
+/*
+create table events(
+	id INT NOT NULL auto_increment,
+    org_name VARCHAR(255),
+    event_name VARCHAR(255),
+    contacts VARCHAR(255),
+    start_date datetime,
+    end_date datetime,
+    PRIMARY KEY (id)
+);
+*/
 $pdo = null;
 $stmt = null;
