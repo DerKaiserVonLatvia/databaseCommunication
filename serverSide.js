6d656ebc6ce0d21620ad36ca741d04cf788c596a
KleinerKaiser <85960973+DerKaiserVonLatvia@users.noreply.github.com>
2023-03-10 17:16:36 +0200

    removed shit

--- serverSide.js
+++ serverSide.js
@@ -9,7 +9,6 @@ http.createServer((request, response) => {
     body.push(chunk);
   }).on('end', () => {
     body = Buffer.concat(body).toString();
-    // BEGINNING OF NEW STUFF
 
     response.on('error', (err) => {
       console.error(err);
@@ -17,16 +16,11 @@ http.createServer((request, response) => {
 
     response.statusCode = 200;
     response.setHeader('Content-Type', 'application/json');
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.writeHead(200, {'Content-Type': 'application/json'})
 
     const responseBody = { headers, method, url, body };
 
     response.write(JSON.stringify(responseBody));
     response.end();
-    // Note: the 2 lines above could be replaced with this next one:
-    // response.end(JSON.stringify(responseBody))
-
-    // END OF NEW STUFF
+    
   });
 }).listen(8080);
